e81db00f654a53a30181f94958016bfcdeeb55bc
Michiel Stragier <michiel.stragier@student.howest.be>
2023-12-18 11:31:04 +0100

    add google maps navigation button

---
+++ src/app/components/GoogleMapsNavigateToButton/GoogleMapsNavigateToButton.js
@@ -0,0 +1,10 @@
+import styles from "./googleMapsNavigateToButton.module.css"
+export default function GoogleMapsNavigateToButton({ location }) {
+    const navigate = () => {
+        window.open(`https://www.google.com/maps/dir/?api=1&destination=${location.lat},${location.lng}`, "_blank")
+    }
+
+    return (
+            <button onClick={navigate} className={styles.button}>Navigeer naar parking</button>
+    )
+}
--- src/app/ticket/page.js
+++ src/app/ticket/page.js
@@ -8,12 +8,25 @@ import MockData from "@/app/mockData/mockData"
 
 
 import styles from "./ticket.module.css"
+import GoogleMapsNavigateToButton from "../components/GoogleMapsNavigateToButton/GoogleMapsNavigateToButton";
 
 
-export default function Ticket({currentSession = MockData.account.currentSession}) {
+export default function Ticket({ currentSession = MockData.account.currentSession }) {
 
     const onMarkerClick = () => {
-        console.log("marker clicked")
+        console.log("marker clicked");
+    }
+
+    if (!currentSession) {
+        return (
+            <>
+                <main>
+                    <h1>Ticket</h1>
+                    <p>Er is momenteel geen actieve sessie.</p>
+                </main>
+                <BottomNavigation />
+            </>
+        );
     }
 
     return (
@@ -22,12 +35,12 @@ export default function Ticket({currentSession = MockData.account.currentSession
                 <h1>Ticket</h1>
                 <div className={styles["flex-container"]}>
                     <SessionCard session={currentSession} />
-                    <PriceList priceRate={currentSession.parking.priceRates} currency={currentSession.parking.currency} classname={styles["prijzen-card"]} title="prijzen" />
+                    <PriceList priceRate={currentSession.parking.priceRates} currency={currentSession.parking.currency} classname={styles["prijzen-card"]} title="Prijzen" />
                 </div>
+                <GoogleMapsNavigateToButton location={currentSession.parking.location} />
                 <MapBox currentSession={currentSession} onMarkerClick={onMarkerClick}/>
-
             </main>
             <BottomNavigation />
         </>
-    )
+    );
 }
