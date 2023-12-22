e5b8163e8d1bb2ea71bdf36484a8af1a6d478913
Michiel Stragier <michiel.stragier@student.howest.be>
2023-12-22 22:27:22 +0100

    add general information to parking

--- src/app/search/page.js
+++ src/app/search/page.js
@@ -3,18 +3,20 @@ import { useState } from "react"
 import BottomNavigation from "../components/BottomNavigation/BottomNavigation"
 import MapBox from "../components/MapBox/MapBox"
 import PriceList from "../components/PriceList/PriceList"
+import styles from "./search.module.css"
 
 import MockData from "@/app/mockData/mockData"
 import GoogleMapsNavigateToButton from "../components/GoogleMapsNavigateToButton/GoogleMapsNavigateToButton";
+import Card from "../components/Card/Card";
 
 export default function Search(){
 
     const [marker, setMarker] = useState(null)
-    const [showPriceList, setShowPriceList] = useState(false)
+    const [showInformation, setShowInformation] = useState(false)
 
     const onMarkerClick = (marker) => {
         setMarker(marker);
-        setShowPriceList(true);
+        setShowInformation(true);
     }
 
     return(
@@ -25,8 +27,18 @@ export default function Search(){
                 currentSession={MockData.account.currentSession} 
                 onMarkerClick = {onMarkerClick}
             />
-            {showPriceList && <PriceList priceRate ={marker.priceRates}/>}
-            {showPriceList && <GoogleMapsNavigateToButton location={marker.location}/>}
+            <div className={styles["flex-container"]}>
+                {showInformation && <PriceList className={styles["price-list"]} priceRate={marker.priceRates}/>}
+                {showInformation && <Card className={styles["general-information-card"]} title={"General information"}> 
+                        <GoogleMapsNavigateToButton location={marker.location}/>    
+                        <ul>
+                            <li>Address: <p>Anne Frankstraat 17, 8870 Izegem, West Flanders, Belgium</p></li>
+                            <li>Parkeer plaatsen: <p>150</p></li>
+                            <li>Vrije plaatsen: <p>29</p></li>
+                        </ul>
+                    </Card>}
+            </div>
+            
         </main>
         <BottomNavigation />
         </>
